dbd123a1fb1968bb14c6515dbcdbee6d80437856
Ravisindhav <sindhavravi11@gmail.com>
2025-07-08 02:21:40 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6152562853:AAGimPmtvHjqcE8em9iDMH-QAjkM8133P0c"
-sudo_groups = -1002049391187
+api_id = 27660379
+api_hash = "19c71c27733f0954371085198855125a"
+bot_token = "7971998323:AAHTuNTLIL5PaQElqpLMhiun8axj1ucFsDY"
+sudo_groups = -1002761572365
