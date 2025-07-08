76e10207105cdafa058137a5f68d01478619364e
Ravisindhav <sindhavravi11@gmail.com>
2025-07-08 13:23:58 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 27660379
 api_hash = "19c71c27733f0954371085198855125a"
 bot_token = "7971998323:AAHTuNTLIL5PaQElqpLMhiun8axj1ucFsDY"
-sudo_groups = [-1002761572365]  # Use list if needed in loops or checks
+sudo_groups = os.environ.get("LOG_CHANNEL")
